f4d7aa12bd8bebdb456669872c8dd53bc2a0218c
J. TURNER <student@epicodus.com>
2015-01-19 15:55:59 -0800

    Modify files.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -19,7 +19,7 @@ describe(Task) do
   end
 
   describe("#description") do
-    it("lets you give it a description") do
+    it("allows for a description") do
       test_task = Task.new({:description => "water plants"})
       expect(test_task.description()).to(eq("water plants"))
     end
@@ -30,6 +30,14 @@ describe(Task) do
       task1 = Task.new({:description => "learn SQL"})
       task2 = Task.new({:description => "learn SQL"})
       expect(task1).to(eq(task2))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn SQL"})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
     end
   end
 
